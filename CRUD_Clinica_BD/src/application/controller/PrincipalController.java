96e508395ab5139c7cc24a58b8c168a5fb90e849
ThaisScheiner <thais.silva124@fatec.sp.gov.br>
2022-06-07 21:34:21 -0300

    juncao completa
    erro nas tabs medico/paciente no scene

--- CRUD_Clinica_BD/src/application/controller/PrincipalController.java
+++ CRUD_Clinica_BD/src/application/controller/PrincipalController.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 
 import javax.swing.JOptionPane;
 
+import application.model.Consulta;
 import application.model.Exame;
 import application.model.Fatura;
 import application.model.Medico;
@@ -145,6 +146,30 @@ public class PrincipalController {
 	private Label lblNomeMedico;
 	@FXML
 	private TextField txtIDMedico;
+	@FXML
+	private TextField tfCodigoConsulta;
+	@FXML
+	private TextField tfNomeConsulta;
+	@FXML
+	private TextField tfDataConsulta;
+	@FXML
+	private TextField tfIDExame;
+	@FXML
+	private TextField tfIDMedico;
+	@FXML
+	private Button btnBuscarConsulta;
+	@FXML
+	private Button btnInserirConsulta;
+	@FXML
+	private Button btnAtualizarConsulta;
+	@FXML
+	private Button btnExcluirConsulta;
+	@FXML
+	private Button btnListarConsulta;
+	@FXML
+	private TextArea taListaConsultas;
+	@FXML
+	private Button btnCopiaConsulta;
 	
 
 	// Event Listener on TextField[#txtFatNomePaciente].onAction
@@ -393,6 +418,61 @@ public class PrincipalController {
 			}
 		}
 	}
+	
+	// Event Listener on TextField[#tfCodigoConsulta].onAction
+		@FXML
+		public void acaoConsulta(ActionEvent event) {
+			String cmd = event.getSource().toString();
+			ConsultaController control = new ConsultaController(tfCodigoConsulta, tfNomeConsulta, tfDataConsulta,
+					           tfIDExame, tfIDMedico, taListaConsultas);	
+			
+			if ((cmd.contains("Inserir")|| cmd.contains("Atualizar"))
+			&& (tfCodigoConsulta.getText().isEmpty())
+			|| tfNomeConsulta.getText().isEmpty()
+			|| tfDataConsulta.getText().isEmpty()
+			|| tfIDExame.getText().isEmpty()
+			|| tfIDMedico.getText().isEmpty()) {
+				JOptionPane.showMessageDialog(null, "Preencha os campos", "ERRO", JOptionPane.ERROR_MESSAGE);
+			} else {
+				if (cmd.contains("Excluir") || cmd.contains("Buscar") || cmd.contains("tfCodigoConsulta")
+					&& tfCodigoConsulta.getText().isEmpty()){
+						JOptionPane.showMessageDialog(null, "Preencha o código", "ERRO", JOptionPane.ERROR_MESSAGE);
+					} else
+						try {
+							if(cmd.contains("Listar")){
+							control.buscarConsultas();		
+						} else {
+							Consulta c = new Consulta();
+							c.setConsID(Integer.parseInt(tfCodigoConsulta.getText()));
+							c.setConsNome(tfNomeConsulta.getText());
+							c.setConsIdExame(Integer.parseInt(tfIDExame.getText()));
+							c.setConsIdMedico(Integer.parseInt(tfIDMedico.getText()));
+							if (cmd.contains("Inserir")) {
+								control.inserirConsulta(c);
+							} else if (cmd.contains("Atualizar")) {
+								control.atualizarConsulta(c);
+							} else if (cmd.contains("Excluir")) {
+								control.excluirConsulta(c);
+							} else if (cmd.contains("Buscar") || cmd.contains("tfCodigoConsulta")) {
+								control.buscarConsulta(c);
+							}
+						}
+			} catch (ClassNotFoundException | SQLException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "ERRO", JOptionPane.ERROR_MESSAGE);
+			}
+			}
+		}
+		
+		@FXML
+		public void copiaConsulta(ActionEvent event) {
+			if (tfCodigoConsulta.getText().isEmpty() || tfNomeConsulta.getText().isEmpty()){
+				JOptionPane.showMessageDialog(null, "Preencha os campos", "ERRO", JOptionPane.ERROR_MESSAGE);
+			} else {
+				tfCodigoConsulta.setText(tfCodigoConsulta.getText());
+				tfNomeConsulta.setText(tfNomeConsulta.getText());
+			}
+			
+		}
 	
 	@FXML
 	public void copiaPaciente(ActionEvent event) 
